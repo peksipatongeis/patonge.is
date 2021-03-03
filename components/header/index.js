4d7c268f431645516942c87ae8ed59a6ef253c08
Pekka Wallenius <pekkawa@gmail.com>
2021-03-03 22:48:41 +0200

    New beginnings

---
+++ components/header/index.js
@@ -0,0 +1,14 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.text}>
+        <div>peksi</div>
+        <div>patongeis</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
