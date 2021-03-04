1b41c202e5893b2f04633e4c546174c338b84851
Pekka Wallenius <pekkawa@gmail.com>
2021-03-04 22:29:35 +0200

    Removed Staatliches font

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Staatliches&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
         />
         <script dangerouslySetInnerHTML={{ __html: randomizerScript }} />
